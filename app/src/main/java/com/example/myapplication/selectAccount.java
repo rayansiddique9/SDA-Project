4ec9d0962a4815b848285604ce706617ad4c2ed6
rayansiddique9 <rayansiddique9@gmail.com>
2022-10-12 08:15:51 -0700

    UniGrab

--- app/src/main/java/com/example/myapplication/selectAccount.java
+++ app/src/main/java/com/example/myapplication/selectAccount.java
@@ -3,33 +3,65 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.helper.widget.Carousel;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class selectAccount extends AppCompatActivity{
 
-  //  private Spinner acc;
-    AutoCompleteTextView auto;
-    ArrayAdapter<String> arr;
-    String []accounts = {"Student", "University"};
+    ArrayAdapter<String> adapter;
+    String []arr = {"Student", "University"};
+    Spinner acc;
+    Button b;
+    String item;
     @Override
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_account);
 
-     /*   acc = findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arr);
+        acc = findViewById(R.id.spinner);
+        b = findViewById(R.id.next);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, arr);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        acc.setAdapter(adapter);*/
+        acc.setAdapter(adapter);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(item == "Student") {
+                    Intent in = new Intent(selectAccount.this, loginCredentialsStudent.class);
+                    startActivity(in);
+                }
+                else if(item == "University")
+                {
+                    Intent in = new Intent(selectAccount.this, loginUni.class);
+                    startActivity(in);
+                }
+            }
+        });
+        acc.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+                  item = parent.getItemAtPosition(i).toString();
+              //  Toast.makeText(getApplicationContext(),"Item: "+item, Toast.LENGTH_SHORT).show();
 
-        auto = findViewById(R.id.act);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+     /*   auto = findViewById(R.id.act);
         arr = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,accounts);
         auto.setAdapter(arr);
         auto.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -39,7 +71,7 @@ public class selectAccount extends AppCompatActivity{
                 Toast.makeText(getApplicationContext(),"Item: "+item, Toast.LENGTH_SHORT).show();
 
             }
-        });
+        });*/
 
 
     }
