cdbb34e0e83e9a0e882da46f74110f922bdcbf0d
rayansiddique9 <rayansiddique9@gmail.com>
2022-10-16 22:08:09 -0700

    UniGrab

--- app/src/main/java/com/example/myapplication/homePage.java
+++ app/src/main/java/com/example/myapplication/homePage.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class loginCredentialsStudent extends AppCompatActivity {
+public class homePage extends AppCompatActivity {
 
-    Button l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_credentials_student);
-
+        setContentView(R.layout.activity_home_page);
     }
 }
