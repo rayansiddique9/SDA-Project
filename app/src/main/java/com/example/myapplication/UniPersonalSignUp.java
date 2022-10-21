4d08c48be2b3f17ccdbe68595f11ad6bf6676d21
Mujahid <mujahidnadeem6@gmail.com>
2022-10-21 11:19:14 +0500

    mujahid work

---
+++ app/src/main/java/com/example/myapplication/UniPersonalSignUp.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UniPersonalSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uni_personal_sign_up);
+    }
+}
