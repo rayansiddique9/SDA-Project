bb2e96ef68f0f7339d6ea7141a8844922208b7f2
AemonFatima <117483315+AemonFatima@users.noreply.github.com>
2022-11-23 00:20:29 +0500

    ok!!hmmm

---
+++ app/src/main/java/com/example/myapplication/activity_student_profile.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class activity_student_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_profile);
+
+        EditText _firstname = findViewById(R.id.firstname);
+        EditText _lastname = findViewById(R.id.lastname);
+        EditText _DOB = findViewById(R.id.DOB);
+        EditText _subjectCombo = findViewById(R.id.subjectcombo);
+        EditText _location = findViewById(R.id.location);
+        EditText _longitude = findViewById(R.id.longitude);
+        EditText _latitude = findViewById(R.id.latitude);
+        EditText _equivalence = findViewById(R.id.equivalence);
+        EditText _cgpa = findViewById(R.id.CGPA);
+    }
+}
